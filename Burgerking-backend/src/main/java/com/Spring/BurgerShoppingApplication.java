4241d3e62aaa9f792541ff5a1f58ffd44061d183
RohanJadhav12 <139950992+RohanJadhav12@users.noreply.github.com>
2023-09-13 08:04:50 +0530

    Add files via upload

---
+++ Burgerking-backend/src/main/java/com/Spring/BurgerShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerShoppingApplication.class, args);
+	}
+
+}
